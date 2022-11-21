d725bad56a3e47096b973b000fc671bd10e5ea7f
fahime khazaei <fh.khazaei@dmiot.ir>
2022-11-21 16:32:12 +0330

    fix:fixed sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -45,8 +45,8 @@ export function Sidebar() {
       </div>
       <Menu
         theme="dark"
-        defaultOpenKeys={["sub1"]}
-        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub3"]}
+        selectedKeys={["sub3"]}
         mode="inline"
         items={items}
       />
